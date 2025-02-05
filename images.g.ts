0b5508daa4c90059bb07c5b78fd2d22c16ab1f80
FrechesMaedchen <micaruehle@gmail.com>
2025-02-05 16:34:43 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "meinBild":return img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 1 1 1 9 9 9 9 9 9 9 9 1 1 
+9 9 1 1 1 1 1 9 9 9 9 9 9 1 1 1 
+9 1 1 1 1 1 1 1 9 9 9 9 1 1 1 1 
+9 1 1 1 1 1 1 1 9 9 9 9 1 1 1 1 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 7 7 7 
+9 9 9 9 9 9 9 9 9 9 9 9 9 7 7 7 
+9 9 9 9 9 9 9 9 9 9 9 9 9 7 7 7 
+7 7 7 9 9 9 9 9 9 9 9 9 9 7 7 7 
+7 7 7 9 9 9 9 9 9 9 9 9 9 9 9 e 
+7 7 7 9 9 9 9 9 9 9 9 9 9 9 9 e 
+9 e 9 9 9 9 9 9 7 7 7 9 9 9 9 e 
+9 e 9 9 9 9 9 7 2 7 7 7 9 9 9 e 
+9 e 9 9 9 9 9 7 7 7 2 7 9 9 9 e 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
---
+++ main.ts
@@ -0,0 +1,7 @@
+namespace SpriteKind {
+    export const FrechesKind = SpriteKind.create()
+}
+let Plartzhalter = 0
+scene.setBackgroundColor(9)
+info.changeCountdownBy(90)
+Plartzhalter += 100
